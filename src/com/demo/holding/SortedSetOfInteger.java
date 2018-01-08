21f73cd2dc6d69a4ed407d74b91a847b77cbfa5d
pj.y <peijiang.yang@gmail.com>
2018-01-08 14:14:16 +0800

    update

--- src/com/demo/holding/SortedSetOfInteger.java
+++ src/com/demo/holding/SortedSetOfInteger.java
@@ -1,7 +1,19 @@
 package com.demo.holding;
 
+import java.util.Random;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 /**
  * Created on 2018/1/8.
  */
 public class SortedSetOfInteger {
+    public static void main(String[] args) {
+        Random random = new Random(47);
+        SortedSet<Integer> intSet = new TreeSet<Integer>();
+        for (int i = 0; i < 10; i++) {
+            intSet.add(random.nextInt(30));
+        }
+        System.out.println(intSet);
+    }
 }
--- src/com/demo/holding/Statistics.java
+++ src/com/demo/holding/Statistics.java
@@ -1,7 +1,21 @@
 package com.demo.holding;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 /**
  * Created on 2018/1/8.
  */
 public class Statistics {
+    public static void main(String[] args) {
+        Random random = new Random(47);
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < 10000; i++) {
+            int r = random.nextInt(20);
+            Integer freq = map.get(r);
+            map.put(r, freq == null ? 1 : freq + 1);
+        }
+        System.out.println(map);
+    }
 }
