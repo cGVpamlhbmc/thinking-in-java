6a2b1501cd938eac917dda190ba1b24303adca65
pj.y <peijiang.yang@gmail.com>
2018-01-09 09:53:35 +0800

    update

---
+++ src/com/demo/holding/PriorityQueueDemo.java
@@ -0,0 +1,12 @@
+package com.demo.holding;
+
+import java.util.PriorityQueue;
+
+/**
+ * Created on 2018/1/8.
+ */
+public class PriorityQueueDemo {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+    }
+}
