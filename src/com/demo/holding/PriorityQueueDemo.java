99c952801c6534e5998c2a4281b8c15fe35d7538
pj.y <peijiang.yang@gmail.com>
2018-01-10 16:31:07 +0800

    update

--- src/com/demo/holding/PriorityQueueDemo.java
+++ src/com/demo/holding/PriorityQueueDemo.java
@@ -1,6 +1,6 @@
 package com.demo.holding;
 
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * Created on 2018/1/8.
@@ -8,5 +8,19 @@ import java.util.PriorityQueue;
 public class PriorityQueueDemo {
     public static void main(String[] args) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Random random = new Random(47);
+        for (int i = 0; i < 10; i++) {
+            pq.offer(random.nextInt(i + 10));
+        }
+        QueueDemo.printQ(pq);
+
+        List<Integer> ints = Arrays.asList(25, 22, 20, 18, 14, 9, 1, 1, 2, 3, 9, 14, 18, 21, 23, 25);
+        pq = new PriorityQueue<Integer>(ints);
+        QueueDemo.printQ(pq);
+        pq = new PriorityQueue<Integer>(ints.size(), Collections.reverseOrder());
+        pq.addAll(ints);
+        QueueDemo.printQ(pq);
+
+
     }
 }
