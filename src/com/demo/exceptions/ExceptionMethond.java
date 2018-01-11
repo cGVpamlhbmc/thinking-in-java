ca6b1b03f0a01eb5d7a933de589509344dfe234e
pj.y <peijiang.yang@gmail.com>
2018-01-11 16:20:59 +0800

    exception method

--- src/com/demo/exceptions/ExceptionMethond.java
+++ src/com/demo/exceptions/ExceptionMethond.java
@@ -4,4 +4,16 @@ package com.demo.exceptions;
  * Created on 2018/1/11.
  */
 public class ExceptionMethond {
+    public static void main(String[] args) {
+        try {
+            throw new Exception("My Exception");
+        } catch (Exception e) {
+            System.out.println("Caught Exception");
+            System.out.println("getMessage(): " + e.getMessage());
+            System.out.println("getLocalizedMessage(): " + e.getLocalizedMessage());
+            System.out.println("toString(): " + e);
+            System.out.print("printStackTrace(): ");
+            e.printStackTrace(System.out);
+        }
+    }
 }
