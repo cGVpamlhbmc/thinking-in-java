541791e2e875014b3e9cd702c833a71772fccd77
pj.y <peijiang.yang@gmail.com>
2018-01-15 00:49:16 +0800

    immutable string

---
+++ src/com/demo/strings/Immutable.java
@@ -0,0 +1,18 @@
+package com.demo.strings;
+
+/**
+ * Created on 2018/1/15.
+ */
+public class Immutable {
+    public static String upcase(String s) {
+        return s.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        String q = "howdy";
+        System.out.println(q);
+        String qq = upcase(q);
+        System.out.println(qq);
+        System.out.println(q);
+    }
+}
