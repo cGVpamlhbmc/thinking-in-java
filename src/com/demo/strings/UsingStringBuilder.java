c899a640a7f867e442640b04b0b3f57cb278a4ee
pj.y <peijiang.yang@gmail.com>
2018-01-15 10:47:09 +0800

    use StringBuilder

---
+++ src/com/demo/strings/UsingStringBuilder.java
@@ -0,0 +1,26 @@
+package com.demo.strings;
+
+import java.util.Random;
+
+/**
+ * Created on 2018/1/15.
+ */
+public class UsingStringBuilder {
+    public static Random random = new Random(47);
+
+    public String toString() {
+        StringBuilder result = new StringBuilder("[");
+        for (int i = 0; i < 25; i++) {
+            result.append(random.nextInt(100));
+            result.append(", ");
+        }
+        result.delete(result.length() - 2, result.length());
+        result.append("]");
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        UsingStringBuilder usb = new UsingStringBuilder();
+        System.out.println(usb.toString());
+    }
+}
