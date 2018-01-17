abb6996ff44c3e6203e5233a7e574068400a7f83
pj.y <peijiang.yang@gmail.com>
2018-01-17 13:32:03 +0800

    generic methods

---
+++ src/com/demo/generics/GenericMethods.java
@@ -0,0 +1,20 @@
+package com.demo.generics;
+
+/**
+ * Created on 2018/1/17.
+ */
+public class GenericMethods {
+    public <T> void f(T x) {
+        System.out.println(x.getClass().getName());
+    }
+
+    public static void main(String[] args) {
+        GenericMethods gm = new GenericMethods();
+        gm.f("");
+        gm.f(1);
+        gm.f(1.0);
+        gm.f(1.0f);
+        gm.f('c');
+        gm.f(gm);
+    }
+}
