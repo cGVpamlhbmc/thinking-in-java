17ad7d2e7dba608397d56a78adf197664242d747
pj.y <peijiang.yang@gmail.com>
2018-01-24 23:00:27 +0800

    selfbound

---
+++ src/com/demo/generics/OrdinaryArguments.java
@@ -0,0 +1,32 @@
+package com.demo.generics;
+
+/**
+ * Created on 2018/1/24.
+ */
+class Base {
+}
+
+class Derived extends Base {
+}
+
+class OrdinarySetter {
+    void set(Base base) {
+        System.out.println("OrdinarySetter.set(Base)");
+    }
+}
+
+class DerivedSetter extends OrdinarySetter {
+    void set(Derived derived) {
+        System.out.println("DerivedSetter.set(Derived)");
+    }
+}
+
+public class OrdinaryArguments {
+    public static void main(String[] args) {
+        Base base = new Base();
+        Derived derived = new Derived();
+        DerivedSetter ds = new DerivedSetter();
+        ds.set(derived);
+        ds.set(base);
+    }
+}
