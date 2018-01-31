c41b0b942b5712f11b63a3f8a52a9fd7c8cbbd88
pj.y <peijiang.yang@gmail.com>
2018-01-31 16:11:44 +0800

    array options

---
+++ src/com/demo/arrays/ContainerComparsion.java
@@ -0,0 +1,41 @@
+package com.demo.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created on 2018/1/31.
+ */
+
+class BerylliumSphere {
+    private static long counter;
+    private final long id = counter++;
+
+    public String toString() {
+        return "Sphere " + id;
+    }
+}
+
+public class ContainerComparsion {
+    public static void main(String[] args) {
+        BerylliumSphere[] spheres = new BerylliumSphere[10];
+        for (int i = 0; i < 5; i++) {
+            spheres[i] = new BerylliumSphere();
+        }
+        System.out.println(Arrays.toString(spheres));
+        System.out.println(spheres[4]);
+
+        List<BerylliumSphere> sphereList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            sphereList.add(new BerylliumSphere());
+        }
+        System.out.println(sphereList);
+        System.out.println(sphereList.get(4));
+
+        List<Integer> intList = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5));
+        intList.add(97);
+        System.out.println(intList);
+        System.out.println(intList.get(4));
+    }
+}
