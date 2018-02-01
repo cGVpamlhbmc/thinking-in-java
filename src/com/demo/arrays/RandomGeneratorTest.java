567b7c64e4f4be65c7285fc0de0db14c9e26e918
pj.y <peijiang.yang@gmail.com>
2018-02-01 10:23:20 +0800

    generator

---
+++ src/com/demo/arrays/RandomGeneratorTest.java
@@ -0,0 +1,12 @@
+package com.demo.arrays;
+
+import net.mindview.util.RandomGenerator;
+
+/**
+ * Created on 2018/2/1.
+ */
+public class RandomGeneratorTest {
+    public static void main(String[] args) {
+        GeneratorTest.test(RandomGenerator.class);
+    }
+}
