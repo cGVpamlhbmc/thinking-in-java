9de2d7e1b28babb0a7f427c66c4a6934b8e20d71
pj.y <peijiang.yang@gmail.com>
2018-03-19 17:27:52 +0800

    enumset

---
+++ src/com/demo/enumerated/AlarmPoints.java
@@ -0,0 +1,8 @@
+package com.demo.enumerated;
+
+/**
+ * Created on 2018/3/19.
+ */
+public enum AlarmPoints {
+    STAIR1, STAIR2, LOBBY, OFFICE1, OFFICE2, OFFICE3, OFFICE4, BATHROOM, UTILITY, KITCHEN
+}
---
+++ src/com/demo/enumerated/SecurityCategory.java
@@ -0,0 +1,32 @@
+package com.demo.enumerated;
+
+import net.mindview.util.Enums;
+
+/**
+ * Created on 2018/3/19.
+ */
+public enum SecurityCategory {
+    STOCK(Security.Stock.class), BOND(Security.Bond.class);
+    Security[] values;
+
+    SecurityCategory(Class<? extends Security> kind) {
+        values = kind.getEnumConstants();
+    }
+
+    interface Security {
+        enum Stock implements Security {SHORT, LONG, MARGIN}
+
+        enum Bond implements Security {MUNICIPAL, JUNK}
+    }
+
+    public Security randomSelection() {
+        return Enums.random(values);
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            SecurityCategory category = Enums.random(SecurityCategory.class);
+            System.out.println(category + ": " + category.randomSelection());
+        }
+    }
+}
