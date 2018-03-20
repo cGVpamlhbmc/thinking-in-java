522868016167ec9cf8074f595dbde5b62bc1b676
pj.y <peijiang.yang@gmail.com>
2018-03-20 16:08:19 +0800

    sleep & priority

---
+++ src/com/demo/concurrency/SleepingTask.java
@@ -0,0 +1,32 @@
+package com.demo.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created on 2018/3/20.
+ */
+public class SleepingTask extends LiftOff {
+    public void run() {
+        try {
+            while (countDown-- > 0) {
+                System.out.println(status());
+                // Old style
+                // Thread.sleep(100);
+                // Java SE5/6 style
+                TimeUnit.MILLISECONDS.sleep(100);
+            }
+        } catch (InterruptedException e) {
+            System.err.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+            exec.execute(new SleepingTask());
+        }
+        exec.shutdown();
+    }
+}
