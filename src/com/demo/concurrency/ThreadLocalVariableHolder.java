dd46da036c3de5061b38146b3283e8b0c0ec4efe
pj.y <peijiang.yang@gmail.com>
2018-03-22 15:26:05 +0800

    threadlocal

---
+++ src/com/demo/concurrency/ThreadLocalVariableHolder.java
@@ -0,0 +1,58 @@
+package com.demo.concurrency;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created on 2018/3/22.
+ */
+
+class Accessor implements Runnable {
+    private final int id;
+
+    public Accessor(int idn) {
+        this.id = idn;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            ThreadLocalVariableHolder.increment();
+            System.out.println(this);
+            Thread.yield();
+        }
+    }
+
+    public String toString() {
+        return "#" + id + ": " + ThreadLocalVariableHolder.get();
+    }
+}
+
+public class ThreadLocalVariableHolder {
+    private static ThreadLocal<Integer> value = new ThreadLocal<Integer>() {
+        private Random random = new Random(47);
+
+        protected synchronized Integer initialValue() {
+            return random.nextInt(10000);
+        }
+    };
+
+    public static void increment() {
+        value.set(value.get() + 1);
+    }
+
+    public static int get() {
+        return value.get();
+    }
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+            exec.execute(new Accessor(i));
+        }
+        TimeUnit.SECONDS.sleep(1);
+        exec.shutdown();
+    }
+}
